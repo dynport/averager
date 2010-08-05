0f293f0f7177842ffdaceb9534927b432b57d30a
Tobias Schwab <tobias.schwab@dynport.de>
2010-08-05 15:43:44 +0200

    add first couple of specs able to log average to file add percentage and expected to log output

--- lib/averager.rb
+++ lib/averager.rb
@@ -2,20 +2,32 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 class Averager
-  VERSION = '0.0.2'
+  VERSION = '0.0.3'
   
   def initialize(options = {})
     @started = Time.now
     @every = options[:every] || 1000
-    if @expected_lines = options[:expected_lines]
-      @digits = @expected_lines.to_i.to_s.length
+    if @expected = options[:expected]
+      @digits = @expected.to_i.to_s.length
     end
     if options[:digits]
       @digits = options[:digits]
     else
       @digits ||= 7
     end
+    @log_path = options[:log_path]
     @stream = options[:stream] || STDOUT
+    flush_stream_for_progress
+    @progress_bar = options[:progress_bar] == true
+  end
+  
+  def flush_stream_for_progress
+    if @log_path
+      FileUtils.mkdir_p(File.dirname(@log_path))
+      @stream = File.open(@log_path, "w")
+    else
+      @stream.print "\r"
+    end
   end
 
   def avg(i, status = nil)
@@ -25,14 +37,28 @@ class Averager
       if block_given?
         out = yield(:digits => @digits, :i => i, :per_second => per_second, :status => status)
       else
-        out = "%0#{@digits}d: %.1f" % [i, per_second]
+        out = "%#{@digits}d" % i
+        if @expected
+          out << "/#{@expected}" 
+          out << " %3.1f%" % (100 * (i / @expected.to_f)) if i <= @expected
+        end
+        out << " (%.1f)" % per_second
         out << ": #{status}" if status
       end
-      @stream.puts out
+      if @progress_bar
+        flush_stream_for_progress
+        @stream.print out
+      else
+        @stream.puts out
+      end
       @stream.flush
       true
     else
       false
     end
+  end
+  
+  def finish
+    @stream.close
   end
 end
--- spec/averager_spec.rb
+++ spec/averager_spec.rb
@@ -3,9 +3,52 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 # Time to add your specs!
 # http://rspec.info/
 describe "Place your specs here" do
-  
-  it "find this spec in spec directory" do
-    # violated "Be sure to write your specs"
+  before(:each) do
+    @log_path = File.expand_path(File.dirname("#{__FILE__}") + "/../tmp/status.log")
+    FileUtils.rm_f(@log_path)
   end
   
+  it "should log to a specific logfile" do
+    TimeTravel.freeze_to Time.local(2009, 9, 9, 11, 20, 0)
+    avg = Averager.new(:log_path => @log_path, :every => 1, :digits => 3)
+    TimeTravel.jump(1)
+    avg.avg(100, "test")
+    TimeTravel.jump(1)
+    avg.avg(300, "test")
+    File.read(@log_path).should == %(100 (100.0): test\n300 (150.0): test\n)
+  end
+  
+  it "should only log every x time" do
+    TimeTravel.freeze_to Time.local(2009, 9, 9, 11, 20, 0)
+    avg = Averager.new(:log_path => @log_path, :every => 600)
+    TimeTravel.jump(1)
+    avg.avg(100, "100")
+    TimeTravel.jump(1)
+    avg.avg(300, "300")
+    TimeTravel.jump(1)
+    avg.avg(600, "600")
+    avg.avg(601, "601")
+    File.read(@log_path).should == %(    600 (200.0): 600\n)
+  end
+  
+  it "should include a percentage and estimated end time" do
+    TimeTravel.freeze_to Time.local(2009, 9, 9, 11, 20, 0)
+    avg = Averager.new(:log_path => @log_path, :expected => 1000, :every => 250)
+    TimeTravel.jump(1)
+    avg.avg(500, "500")
+    TimeTravel.jump(1)
+    avg.avg(750, "750")
+    File.read(@log_path).should == %( 500/1000 50.0% (500.0): 500\n 750/1000 75.0% (375.0): 750\n)
+  end
+  
+  it "should print a progress when asked for" do
+    TimeTravel.freeze_to Time.local(2009, 9, 9, 11, 20, 0)
+    avg = Averager.new(:log_path => @log_path, :expected => 1000, :every => 250, :progress_bar => true)
+    TimeTravel.jump(1)
+    avg.avg(500, "500")
+    File.read(@log_path).should == %( 500/1000 50.0% (500.0): 500)
+    TimeTravel.jump(1)
+    avg.avg(750, "750")
+    File.read(@log_path).should == %( 750/1000 75.0% (375.0): 750)
+  end
 end
